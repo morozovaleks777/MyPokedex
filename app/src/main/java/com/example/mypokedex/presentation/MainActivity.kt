ce185da5aefe258c84bf178a7dfb54f37cfd6b5b
Oleksandr_Morozov <moralex260@gmail.com>
2021-05-05 00:40:54 +0300

    add fragments animation

--- app/src/main/java/com/example/mypokedex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mypokedex/presentation/MainActivity.kt
@@ -1,32 +1,58 @@
 package com.example.mypokedex.presentation
 
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
+import androidx.transition.Slide
 import com.example.mypokedex.DetailFragment
 import com.example.mypokedex.ListFragment
 import com.example.mypokedex.R
 import com.example.mypokedex.data.network.PokemonApiFilter
 
 
-class MainActivity : AppCompatActivity(), Navigation {
+class MainActivity : AppCompatActivity(), Navigation,Navigation2 {
+
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
 
             supportFragmentManager.beginTransaction()
+               .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_right,
+                   R.animator.slide_in_left, R.animator.slide_in_right
+               )
                 .replace(android.R.id.content, ListFragment())
                 .commit()
 
+
         }
 
-        override fun openPokemonDetails(id: String) {
+       override fun openPokemonDetails(id: String) {
             supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_right,
+                    R.animator.slide_in_left, R.animator.card_flip_right_exit)
                 .replace(android.R.id.content, DetailFragment.newInstance(id))
+
                 .addToBackStack(null)
+
                 .commit()
+
+
         }
+    override fun openPokemonList(){
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_right,
+                R.animator.slide_in_left, R.animator.slide_in_right
+            )
+            .replace(R.id.cardView, ListFragment())
+            .commit()
+
+    }
         override fun onCreateOptionsMenu(menu: Menu?): Boolean {
             menuInflater.inflate(R.menu.overflow_menu,menu)
             return super.onCreateOptionsMenu(menu)
@@ -42,11 +68,19 @@ class MainActivity : AppCompatActivity(), Navigation {
             return true
         }
 
+
     }
 
-    interface Navigation {
-        fun openPokemonDetails(id: String)
-    }
+
+
+
+interface Navigation {
+        fun openPokemonDetails(id: String)}
+interface Navigation2{
+    fun openPokemonList()
+}
+
+
 
 
 
