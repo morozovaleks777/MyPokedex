1ccbd4fa6170dc9aae9a62e29bdb44907692b6a0
morozovaleks777 <80290134+morozovaleks777@users.noreply.github.com>
2021-05-17 20:03:55 +0300

    5
    progressbar logic

---
+++ app/src/main/java/com/example/mypokedex/di/Coin.kt
@@ -0,0 +1,31 @@
+package com.example.mypokedex.di
+
+import com.example.mypokedex.DetailViewModel
+import com.example.mypokedex.ListViewModel
+import com.example.mypokedex.data.NetworkPokemonRepository
+import com.example.mypokedex.data.network.PokedexApiService
+import com.example.mypokedex.data.network.createPokedexApiService
+import com.example.mypokedex.domain.PokemonDetails
+import com.example.mypokedex.domain.PokemonRepository
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val appModule = module {
+    single<PokedexApiService> { createPokedexApiService() }
+    single<PokemonRepository> { NetworkPokemonRepository(get()) }
+
+    viewModel { ListViewModel(get()) }
+    viewModel { DetailViewModel(get()) }
+}
+
+//private fun createPokedexApiService(): PokedexApiService {
+//    val retrofit = Retrofit.Builder()
+//        .baseUrl("https://pokeapi.co/api/v2/")
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
+//
+//    return retrofit.create(PokedexApiService::class.java)
+//}
---
+++ app/src/main/java/com/example/mypokedex/domain/PokemonListResponse.kt
@@ -0,0 +1,32 @@
+package com.example.mypokedex.domain
+
+import androidx.core.net.toUri
+
+data class PokemonListResponse(val count : Int, val results : List<PokemonPartialInfo>)
+
+data class PokemonPartialInfo(val name : String, val url : String)
+
+val PokemonPartialInfo.id: String
+    get() = url.toUri().lastPathSegment ?: ""
+
+val PokemonPartialInfo.imageUrl: String
+    get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
+
+
+data class PokemonInfoResponse(
+    val id: String,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val abilities: List<Ability>
+) {
+    data class Ability(
+        val ability: AbilityPartialInfo,
+        val slot: Int
+    )
+
+    data class AbilityPartialInfo(
+        val name: String,
+        val url: String
+    )
+}
